bbfab3dee76d4c90912b26af73041f35aa347a26
jhchen <410861420@qq.com>
2024-04-01 11:26:25 +0800

    [comment] 注释添加

--- packages/components/global.d.ts
+++ packages/components/global.d.ts
@@ -1,7 +1,9 @@
 // GlobalComponents for Volar
+// 指定了模块名为 'vue'，表示对 Vue.js 模块进行扩展。
 declare module 'vue' {
   /**
    * 声明全局组件，便于 volar 识别
+   * 用于声明 Vue.js 中的全局组件，以便 Volar 插件可以正确识别这些组件并提供相应的智能提示和类型检查
    */
   export interface GlobalComponents {
     HuiTest: typeof import('@hua5/hua5-web-ui')['HuiTest']
