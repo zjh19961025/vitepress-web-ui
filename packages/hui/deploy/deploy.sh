4f2c7bcba7e1996246a0f0893f8eeb0fb54e2946
liuhongda <1156614427@qq.com>
2024-05-24 18:36:05 +0800

    update version and publish

--- packages/hui/deploy/deploy.sh
+++ packages/hui/deploy/deploy.sh
@@ -3,5 +3,5 @@ node deploy/update-verson.cjs
 corepack use pnpm@latest
 pnpm eslint --fix package.json
 git add -A
-git commit -m "update version"
+git commit -m "update version and publish"
 pnpm publish
