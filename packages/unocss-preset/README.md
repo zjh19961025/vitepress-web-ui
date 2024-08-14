cc9b772aa72946b84c507548ff5f3a81dffa7832
jhchen <410861420@qq.com>
2024-08-14 14:03:34 +0800

    [unocss-preset] 增加打包处理，用于生成类型说明文件

--- packages/unocss-preset/README.md
+++ packages/unocss-preset/README.md
@@ -1,1 +1,25 @@
-[说明](http://192.168.31.5/front-end/hua5-web-ui/unocssPreset/)
+# [说明文档](http://192.168.31.5/front-end/hua5-web-ui/unocssPreset/)
+
+
+# 特别说明：
+### 导出 和 打包
+导出使用：package.json
+``` json
+{
+"exports": {
+    ".": {
+      "types": "./types/index.d.ts", // 类型声明文件
+      "import": "./src/index.ts" // 源码导出使用
+    },
+    "./presetHui": "./src/preset-hui/index.ts",
+    "./global": {
+      "types": "./global.d.ts"
+    }
+  },
+}
+```
+* 使用的是源码导出使用，打包仅用于生成类型声明文件 types/index.d.ts
+* 如果需要更新 声明文件，那么可运行打包命令
+    ```
+    pnpm build
+    ```
---
+++ packages/unocss-preset/vite.config.js
@@ -0,0 +1,44 @@
+import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+/**
+ * 主要用来生成声明文件
+ * 使用端使用的还是源码引入，可查看 package.json
+ */
+export default defineConfig({
+  plugins: [
+    dts({
+      // 输出目录
+      outDir: ['types', "dist"],
+      // 将动态引入转换为静态（例如：`import('vue').DefineComponent` 转换为 `import { DefineComponent } from 'vue'`）
+      staticImport: true,
+      // 将所有的类型合并到一个文件中
+      rollupTypes: true,
+    }),
+  ],
+  resolve: {
+    // 导入时想要省略的扩展名列表
+    extensions: ['.mjs', '.js', '.mts', '.ts', '.jsx', '.tsx', '.json', '.vue'],
+  },
+  // 打包配置
+  build: {
+    rollupOptions: {
+      // 排除不需要的依赖
+      external: ['vue', 'unocss', "@hua5/hua5-utils"],
+      output: [
+        {
+          // 打包成 es module
+          format: 'es',
+          // 重命名
+          entryFileNames: '[name].js',
+        },
+      ],
+    },
+    // 库打包选项
+    lib: {
+      // 指定入口文件
+      entry: 'src/index.ts',
+      // 组件库名称
+      name: 'unocss-preset',
+    },
+  },
+})
