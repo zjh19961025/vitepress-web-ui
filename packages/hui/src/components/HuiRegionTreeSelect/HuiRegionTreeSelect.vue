21faeae35abc6caa77342fa821c32a5ef1da022a
ziwen <860128394@qq.com>
2024-10-15 18:02:41 +0800

    HuiRegionTreeSelect件文档

--- packages/hui/src/components/HuiRegionTreeSelect/HuiRegionTreeSelect.vue
+++ packages/hui/src/components/HuiRegionTreeSelect/HuiRegionTreeSelect.vue
@@ -1,5 +1,5 @@
 <template>
-  <el-tree-select
+  <ElTreeSelect
     v-model="selectedRegions"
     :data="regionData"
     :filter-node-method="filterNodeMethod"
@@ -8,43 +8,51 @@
     <template #label="{ value }">
       <span>{{ setLabel(value) }}</span>
     </template>
-  </el-tree-select>
+  </ElTreeSelect>
 </template>
 
 <script lang="ts" setup>
-import { ref, watch, toValue, onMounted, nextTick } from 'vue'
+import { ElTreeSelect } from 'element-plus'
+import { ref, watch, onMounted, nextTick } from 'vue'
 import { getRegionNameByCodeWithTree } from '@hua5/hua5-web-lib'
 import type { treeSelectProps } from './type'
 
+defineOptions({
+  name: 'HuiRegionTreeSelect',
+})
+
 // 接收的参数
-const { disabledRegions, treeData } = withDefaults(defineProps<treeSelectProps>(), {
+const props = withDefaults(defineProps<treeSelectProps>(), {
   disabledRegions: () => [],
   selectedRegions: () => [],
 })
 
 const selectedRegions = defineModel<string | string[]>('selectedRegions')
-
-const regionData = ref<any[]>([])
+const allRegionTree = ref([])
 
 onMounted(async() => {
-  regionData.value = treeData || await window.huiDelegate.getRegionTree()
+  allRegionTree.value = await window.huiDelegate.getRegionTree()
 
   // 设置禁用
   nextTick(() => {
-    if (disabledRegions.length > 0 && regionData.value.length > 0) {
+    if (props.disabledRegions.length > 0 && regionData.value.length > 0) {
       setDisAbledRegion(regionData.value)
     }
   })
 })
 
+const regionData = computed(() => {
+  return props.treeData || allRegionTree.value
+})
+
 // 设置label 拼接父级label
 function setLabel(value:string) {
-  return getRegionNameByCodeWithTree(value, regionData.value)
+  return getRegionNameByCodeWithTree(value, allRegionTree.value)
 }
 
 // 设置禁用节点
-watch(disabledRegions, () => {
-  if (disabledRegions.length > 0 && regionData.value.length > 0) {
+watch(() => props.disabledRegions, () => {
+  if (props.disabledRegions.length > 0 && regionData.value.length > 0) {
     setDisAbledRegion(regionData.value)
   }
 }, {
@@ -52,12 +60,13 @@ watch(disabledRegions, () => {
   immediate: true,
 })
 
+// 设置禁用的方法
 function setDisAbledRegion(regions:any) {
-  toValue(regions).forEach((item:any) => {
+  regions.forEach((item:any) => {
     if (item.hasChildren) {
       setDisAbledRegion(item.children)
     } else {
-      item.disabled = disabledRegions?.includes(item.id)
+      item.disabled = props.disabledRegions?.includes(item.id)
     }
   })
 }
