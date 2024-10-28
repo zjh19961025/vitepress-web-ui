fc13dc19accefd9c1213b78243c9d505e65067ed
zhujh <30479317@qq.com>
2024-10-28 17:43:58 +0800

    [HuiGridForm]文档修改

--- docs/examples/HuiGridForm/HuiGridForm.vue
+++ docs/examples/HuiGridForm/HuiGridForm.vue
@@ -15,24 +15,25 @@ const config = ref(
       { label: 'ceshi2', value: 2 },
     ],
     style: 'color:#409EFF;font-weight: 700;',
-    width: '20%',
+    width: '18%',
   },
   {
     prop: 'test2',
     title: '标题2',
     append: '扎',
     placeholder: '请选择',
-    width: '30%',
+    width: '18%',
   },
   {
     prop: 'test3',
     title: '标题3',
     append: '元',
-    width: '20%',
+    width: '18%',
+    style: 'color:#409EFF;font-weight: 700;',
   },
   {
     prop: 'test4',
-    title: '标题4',
+    title: '可以重复选',
     append: '扎',
     placeholder: '请选择',
     type: 'select',
@@ -41,10 +42,26 @@ const config = ref(
       { label: 'ceshi', value: 1 },
       { label: 'ceshi2', value: 2 },
     ],
+    errMsg: 'errMsg',
+    width: '18%',
+    reselected: true,
+  },
+  {
+    prop: 'test5',
+    title: '测试',
+    placeholder: '请选择',
+    type: 'select',
+    dataType: 'text',
+    dict: [
+      { label: 'ceshi', value: 1, disabled: true },
+      { label: 'ceshi2', value: 2 },
+      { label: 'ceshi3', value: 3 },
+    ],
     style: 'color:#409EFF;font-weight: 700;',
     errMsg: 'errMsg',
-    width: '20%',
-  }],
+    width: '18%',
+  },
+  ],
 )
 function getHuiGridFormData() {
   const res = HuiGridFormRef.value.getData()
@@ -53,7 +70,7 @@ function getHuiGridFormData() {
 }
 // 渲染数据 用来设置回显
 const listData = ref([
-  { test1: 1, test2: 2, test3: 3, test4: 4 },
+  { test1: 1, test2: 2, test3: 3, test4: 4, test5: 5 },
 ])
 </script>
 
@@ -65,11 +82,15 @@ const listData = ref([
       hand-sort
       is-can-append
       hand-delete
-      :is-have-operate="true" :config="config" :list-data="listData"
+      :config="config" :list-data="listData"
     >
-      <!--   插槽用法   -->
+      <!--   插槽用法整个替换   -->
       <template #test1="{dataItem,el}">
         <el-input v-model="dataItem[el.prop]" placeholder="插槽的input" />
+      </template>
+      <!--   插槽用法替换select里面的option   -->
+      <template #test5-option="{option, prop}">
+        {{ option.label + " " + (option.isUsed ? "已占用" : (option.disabled ? "不可用" : "")) }}
       </template>
     </HuiGridForm>
     <el-button class="mt-10" @click="getHuiGridFormData()">获取组件数据</el-button>
