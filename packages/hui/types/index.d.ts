41c913632192a2fbd6c8e290824be75ce0a11dfe
yun <2292056558@qq.com>
2024-11-02 10:33:17 +0800

    update version and publish

--- packages/hui/types/index.d.ts
+++ packages/hui/types/index.d.ts
@@ -1102,11 +1102,10 @@ export declare const HuiRegionTreeSelect: SFCWithInstall<DefineComponent<Extract
 export declare const HuiRules: {
     /**
      * 是否url
-     * @param isCanEmpty 是否可以为空，默认不能为空
      * @param tips 错误提示
      */
-    urlRule: (isCanEmpty?: boolean, tips?: string) => {
-        validator: (rule: any, value: any, callback: any) => void;
+    urlRule: (tips?: string) => {
+        validator: (rule: any, value: any, callback: any) => any;
         trigger: string;
     };
     /**
@@ -1114,7 +1113,7 @@ export declare const HuiRules: {
      * @param tips 错误提示
      */
     versionRule: (tips?: string) => {
-        validator: (rule: any, value: any, callback: any) => void;
+        validator: (rule: any, value: any, callback: any) => any;
         trigger: string;
     };
     /**
@@ -1122,7 +1121,7 @@ export declare const HuiRules: {
      * @param tips 错误提示
      */
     jsonRule: (tips?: string) => {
-        validator: (rule: any, value: any, callback: any) => void;
+        validator: (rule: any, value: any, callback: any) => any;
         trigger: string;
     };
     /**
@@ -1150,7 +1149,7 @@ export declare const HuiRules: {
      * @param codeLength 验证码长度
      */
     codeRule: (tips?: string, codeLength?: number) => {
-        validator: (rule: any, value: any, callback: any) => void;
+        validator: (rule: any, value: any, callback: any) => any;
         trigger: string;
     };
     /**
@@ -1158,7 +1157,7 @@ export declare const HuiRules: {
      * @param tips 错误提示
      */
     phoneNumRule: (tips?: string) => {
-        validator: (rule: any, value: any, callback: any) => void;
+        validator: (rule: any, value: any, callback: any) => any;
         trigger: string;
     };
     /**
@@ -1167,8 +1166,8 @@ export declare const HuiRules: {
      * @param max 最大值
      * @param tips 错误提示
      */
-    numRangeRule: (min: number | string, max: number | string, tips: string) => {
-        validator: (rule: any, value: any, callback: any) => void;
+    numRangeRule: (min: any, max: any, tips: any) => {
+        validator: (rule: any, value: any, callback: any) => any;
         trigger: string;
     };
 };
