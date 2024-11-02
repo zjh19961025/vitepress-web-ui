8c848f2ee9a34f3284f5f148932d03761f8378b6
yun <2292056558@qq.com>
2024-11-02 10:30:38 +0800

    [hui-rules] 表单规则去除判空

--- packages/hui/src/utils/hui-rules.ts
+++ packages/hui/src/utils/hui-rules.ts
@@ -3,19 +3,15 @@ import { testUtils } from "@hua5/hua5-utils"
 export const HuiRules = {
   /**
    * 是否url
-   * @param isCanEmpty 是否可以为空，默认不能为空
    * @param tips 错误提示
    */
-  urlRule: (isCanEmpty = false, tips = '请输入正确的链接') => {
+  urlRule: (tips = '请输入正确的链接') => {
     return {
       validator: (rule, value, callback) => {
-        if (isCanEmpty && testUtils.isEmpty(value)) callback()
-        if (testUtils.isNotEmpty(value) && testUtils.isUrl(value)) callback()
-        else {
-          callback(
-            new Error(tips),
-          )
+        if (testUtils.isNotEmpty(value) && !testUtils.isUrl(value)) {
+          return callback(new Error(tips))
         }
+        callback()
       },
       trigger: 'blur',
     }
@@ -28,12 +24,10 @@ export const HuiRules = {
   versionRule: (tips = '请输入正确的版本号') => {
     return {
       validator: (rule, value, callback) => {
-        if (testUtils.isNotEmpty(value) && testUtils.isVersion(value)) callback()
-        else {
-          callback(
-            new Error(tips),
-          )
+        if (testUtils.isNotEmpty(value) && !testUtils.isVersion(value)) {
+          return callback(new Error(tips))
         }
+        callback()
       },
       trigger: 'blur',
     }
@@ -47,7 +41,7 @@ export const HuiRules = {
     return {
       validator: (rule, value, callback) => {
         if (testUtils.isNotEmpty(value) && !testUtils.isJsonString(value)) {
-          callback(new Error(tips))
+          return callback(new Error(tips))
         }
         callback()
       },
@@ -63,18 +57,17 @@ export const HuiRules = {
   intNumRule: (isCanZero = true, isCanNegative = true) => {
     return {
       validator: (rule, value, callback) => {
-        const numVal = Number(value)
-        // 如果不是整数，则返回错误
-        if (!Number.isInteger(numVal) || testUtils.isEmpty(value)) {
-          return callback(new Error('输入的值不能为空且必须是整数'))
-        }
-        // 如果不允许输入 0，且输入的值是 0，则返回错误
-        if (!isCanZero && numVal === 0) {
-          return callback(new Error('输入的值不能为 0'))
-        }
-        // 如果不允许输入负数，且输入的值是负数，则返回错误
-        if (!isCanNegative && numVal < 0) {
-          return callback(new Error('输入的值不能为负数'))
+        if (testUtils.isNotEmpty(value)) {
+          const numVal = Number(value)
+          if (!Number.isInteger(numVal)) {
+            return callback(new Error('输入的值必须是整数'))
+          }
+          if (!isCanZero && numVal === 0) {
+            return callback(new Error('输入的值不能为 0'))
+          }
+          if (!isCanNegative && numVal < 0) {
+            return callback(new Error('输入的值不能为负数'))
+          }
         }
         callback()
       },
@@ -91,22 +84,20 @@ export const HuiRules = {
   digitNumRule: (digit = 2, isCanZero = true, isCanNegative = true) => {
     return {
       validator: (rule, value, callback) => {
-        const numVal = Number(value)
-        // 如果不是数字，则返回错误
-        if (isNaN(numVal) || testUtils.isEmpty(value)) {
-          return callback(new Error('输入的值不能为空且必须是数字'))
-        }
-        // 如果不满足小数位数要求，则返回错误
-        if (value?.toString()?.split('.')?.[1]?.length > digit) {
-          return callback(new Error(`输入的值最多只能有 ${digit} 位小数`))
-        }
-        // 如果不允许输入 0，且输入的值是 0，则返回错误
-        if (!isCanZero && numVal === 0) {
-          return callback(new Error('输入的值不能为 0'))
-        }
-        // 如果不允许输入负数，且输入的值是负数，则返回错误
-        if (!isCanNegative && numVal < 0) {
-          return callback(new Error('输入的值不能为负数'))
+        if (testUtils.isNotEmpty(value)) {
+          const numVal = Number(value)
+          if (isNaN(numVal)) {
+            return callback(new Error('输入的值必须是数字'))
+          }
+          if (value?.toString()?.split('.')?.[1]?.length > digit) {
+            return callback(new Error(`输入的值最多只能有 ${digit} 位小数`))
+          }
+          if (!isCanZero && numVal === 0) {
+            return callback(new Error('输入的值不能为 0'))
+          }
+          if (!isCanNegative && numVal < 0) {
+            return callback(new Error('输入的值不能为负数'))
+          }
         }
         callback()
       },
@@ -122,8 +113,8 @@ export const HuiRules = {
   codeRule: (tips = '请输入正确的验证码', codeLength = 6) => {
     return {
       validator: (rule, value, callback) => {
-        if (testUtils.isEmpty(value) || !testUtils.isCode(value, codeLength)) {
-          callback(new Error(tips))
+        if (testUtils.isNotEmpty(value) && !testUtils.isCode(value, codeLength)) {
+          return callback(new Error(tips))
         }
         callback()
       },
@@ -138,8 +129,8 @@ export const HuiRules = {
   phoneNumRule: (tips = '请输入正确的电话号码') => {
     return {
       validator: (rule, value, callback) => {
-        if (testUtils.isEmpty(value) || !testUtils.isMobile(value)) {
-          callback(new Error(tips))
+        if (testUtils.isNotEmpty(value) && !testUtils.isMobile(value)) {
+          return callback(new Error(tips))
         }
         callback()
       },
@@ -153,14 +144,17 @@ export const HuiRules = {
    * @param max 最大值
    * @param tips 错误提示
    */
-  numRangeRule: (min: number | string, max: number | string, tips: string) => {
+  numRangeRule: (min, max, tips) => {
     return {
       validator: (rule, value, callback) => {
-        if (isNaN(Number(value))) {
-          callback(new Error('请输入数字'))
-        }
-        if (Number(value) < Number(min) || Number(value) > Number(max)) {
-          callback(new Error(tips))
+        if (testUtils.isNotEmpty(value)) {
+          const numVal = Number(value)
+          if (isNaN(numVal)) {
+            return callback(new Error('请输入数字'))
+          }
+          if (numVal < Number(min) || numVal > Number(max)) {
+            return callback(new Error(tips))
+          }
         }
         callback()
       },
