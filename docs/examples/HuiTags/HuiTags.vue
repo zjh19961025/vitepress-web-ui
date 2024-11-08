96f66e562086a9f3944e0e5fd57814c1d59137fa
zhujh <30479317@qq.com>
2024-11-08 14:15:35 +0800

    [doc]HuiTags and HuiTagsMore

---
+++ docs/examples/HuiTags/HuiTags.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="flex">
+    <HuiTags text="红色" type="error" />
+    <HuiTags text="默认" type="primary" />
+    <HuiTags text="成功" type="success" />
+    <HuiTags text="危险" type="warning" />
+    <HuiTags text="信息" type="info" />
+    <HuiTags text="自定义class" tag-class="!bg-red" />
+    <HuiTags text="自定义style" style="background: #0b8235" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ docs/examples/HuiTagsMore/HuiTagsMore.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+const sellerTagSelectorObj = ref({
+  1: {
+    k: '1',
+    v: '圆通',
+    tip: '圆通圆通通圆通',
+    color: '#CA2020',
+  },
+  2: {
+    k: '1',
+    v: '顺丰',
+    tip: '顺丰',
+    color: '#ca20b3',
+  },
+  3: {
+    k: '1',
+    v: '邮政',
+    tip: '邮政',
+    color: '#0b5662',
+  },
+  4: {
+    k: '1',
+    v: '菜鸟',
+    tip: '菜鸟',
+    color: '#c2eeff',
+  },
+})
+</script>
+
+<template>
+  <div class="w-100 p-t-10">
+    <HuiTagsMore show-tips :tags-list="[1,2,3,4]" :dict-obj="sellerTagSelectorObj" />
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
