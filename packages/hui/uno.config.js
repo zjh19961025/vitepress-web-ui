0bd11f7151ac6f44e12cde7021a813a2bce09263
zhujh <30479317@qq.com>
2024-11-15 17:55:04 +0800

    添加预设插件

--- packages/hui/uno.config.js
+++ packages/hui/uno.config.js
@@ -1,4 +1,4 @@
-import { defineConfig } from 'unocss'
+import { defineConfig, transformerDirectives, transformerVariantGroup } from 'unocss'
 // eslint-disable-next-line import/no-unresolved
 import { presetHui } from '@hua5/unocss-preset/presetHui'
 
@@ -6,5 +6,12 @@ export default defineConfig({
   // 预设
   presets: [
     presetHui(),
+  ],
+  // 转换器
+  transformers: [
+    // 指令转换器
+    transformerDirectives(),
+    // 变体组转换器
+    transformerVariantGroup(),
   ],
 })
