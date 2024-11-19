2f72cda42e20936a488ec1ff79e06aa4661d3f20
zhujh <30479317@qq.com>
2024-11-19 17:45:38 +0800

    update version and publish

--- packages/hui/types/index.d.ts
+++ packages/hui/types/index.d.ts
@@ -139,7 +139,6 @@ export declare const HuiAMapSelectAddress: SFCWithInstall<DefineComponent<Extrac
     modelValue: PropType<any>;
     iconPath: {
         type: PropType<string>;
-        default: string;
     };
     value: {
         type: PropType<any>;
@@ -162,7 +161,6 @@ export declare const HuiAMapSelectAddress: SFCWithInstall<DefineComponent<Extrac
     };
     iconClass: {
         type: PropType<string>;
-        default: string;
     };
 }>, {
     loadMap: () => void;
@@ -173,7 +171,6 @@ export declare const HuiAMapSelectAddress: SFCWithInstall<DefineComponent<Extrac
     modelValue: PropType<any>;
     iconPath: {
         type: PropType<string>;
-        default: string;
     };
     value: {
         type: PropType<any>;
@@ -196,15 +193,12 @@ export declare const HuiAMapSelectAddress: SFCWithInstall<DefineComponent<Extrac
     };
     iconClass: {
         type: PropType<string>;
-        default: string;
     };
 }>> & Readonly<{}>, {
-    iconPath: string;
     width: string;
     height: string;
     disabled: boolean;
     showInput: boolean;
-    iconClass: string;
 }, {}, {}, {}, string, ComponentProvideOptions, true, {}, any>> & Record<string, any>;
 
 /**
